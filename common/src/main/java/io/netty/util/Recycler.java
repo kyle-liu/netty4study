f73befd31e6e5ba6f3ac418a9060c944b999d20b
canhun <canhun@taobao.com>
2014-04-24 09:39:45 +0800

    阅读Recycler源代码

--- common/src/main/java/io/netty/util/Recycler.java
+++ common/src/main/java/io/netty/util/Recycler.java
@@ -59,7 +59,8 @@ public abstract class Recycler<T> {
 
     protected abstract T newObject(Handle handle);
 
-    public interface Handle { }
+    public interface Handle {
+    }
 
     static final class Stack<T> implements Handle {
 
@@ -67,47 +68,73 @@ public abstract class Recycler<T> {
 
         final Recycler<T> parent;
         final Thread thread;
-        private T[] elements;
-        private int size;
+        private T[] elements;  //存放元素的数组
+        private int size;  //elements数组中已被赋值的元素中最后一个元素的索引+1即下一个待填元素的索引
+        //该Map主要用来检查Stack中是否有重复元素
         private final Map<T, Boolean> map = new IdentityHashMap<T, Boolean>(INITIAL_CAPACITY);
 
-        @SuppressWarnings({ "unchecked", "SuspiciousArrayCast" })
+        @SuppressWarnings({"unchecked", "SuspiciousArrayCast"})
         Stack(Recycler<T> parent, Thread thread) {
             this.parent = parent;
             this.thread = thread;
+            //初始化数组
             elements = newArray(INITIAL_CAPACITY);
         }
 
+
+        //弹出元素
         T pop() {
             int size = this.size;
+
+            //检查elements中是否有元素，没有元素则直接返回null
             if (size == 0) {
                 return null;
             }
-            size --;
+
+            //索引-1
+            size--;
+            //从数组中取出元素
             T ret = elements[size];
+            //将当前位置置为null
             elements[size] = null;
+            //从map中删除该元素
             map.remove(ret);
             this.size = size;
             return ret;
         }
 
+
+        //往Stack中压入元素
         void push(T o) {
+
+            /**
+             * 使用map.put方法，当put的key-value已存在时，put返回的值不为null
+             * 这里检查添加已存在的值时，thow异常
+             */
             if (map.put(o, Boolean.TRUE) != null) {
                 throw new IllegalStateException("recycled already");
             }
 
             int size = this.size;
+            /**
+             * 检查elements数组容器中已被赋值元素的最大个数，如果发现elements容器已经被填满
+             * 则把elements数组扩容，扩容的长度是：size*2
+             * 然后再把旧的elements元素复制到新的数组中去
+             */
             if (size == elements.length) {
                 T[] newElements = newArray(size << 1);
                 System.arraycopy(elements, 0, newElements, 0, size);
                 elements = newElements;
             }
 
+            //把新添加的元素赋值到elements数组
             elements[size] = o;
+
+            //索引+1
             this.size = size + 1;
         }
 
-        @SuppressWarnings({ "unchecked", "SuspiciousArrayCast" })
+        @SuppressWarnings({"unchecked", "SuspiciousArrayCast"})
         private static <T> T[] newArray(int length) {
             return (T[]) new Object[length];
         }
